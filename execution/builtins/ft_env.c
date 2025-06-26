9e19045985540fc6bcca391b9a33cc05fc2b2e5c
Anass Amajane <anaamaja@e2r5p12.1337.ma>
2025-06-26 15:05:50 +0100

    from **envp to t_env *

--- execution/builtins/ft_env.c
+++ execution/builtins/ft_env.c
@@ -1,11 +1,31 @@
 #include "../exec.h"
 
+int	ft_env(char **argv, t_env *env)
+{
+	if (argv[1])
+	{
+		write (2, "bash: env: too many arguments\n", 31);
+		return (1);
+	}
+	while (env)
+	{
+		if (env->value)
+			printf("%s=%s\n", env->key, env->value);
+		env = env->next;
+	}
+	return (0);
+}
+
+/*
 int	ft_env(char **argv, char **envp)
 {
 	int	i;
 
 	if (argv[1])
+	{
+		write (2, "bash: env: too many arguments\n", 31);
 		return (1);
+	}
 	i = 0;
 	while (envp[i])
 	{
@@ -26,7 +46,7 @@ int	main(int ac, char **av, char **envp)
 	for (int i = 0; args[i]; i++)
 		free(args[i]);
 	free(args);
-}
+}*/
 
 /*int	main(int ac, char **av, char **envp)
 {
